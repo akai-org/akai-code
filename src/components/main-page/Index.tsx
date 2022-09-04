e79b81407718d251340ed38cd2039dc221db4575
roertbb <robert.banaszak1997@gmail.com>
2022-09-04 14:04:28 +0200

    update title and favicon

--- src/components/main-page/Index.tsx
+++ src/components/main-page/Index.tsx
@@ -1,10 +1,15 @@
 import { Hero } from "./Hero/Hero";
 import { EventDetails } from "./EventDetails/EventDetails";
 import { Registration } from "./Registration/Registration";
+import Head from "next/head";
 
 export function Index() {
   return (
     <>
+      <Head>
+        <title>AKAI Code</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Hero />
       <EventDetails />
       <Registration />
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,10 +5,6 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <meta
-            name="viewport"
-            content="initial-scale=1.0, width=device-width"
-          />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
             rel="preconnect"
