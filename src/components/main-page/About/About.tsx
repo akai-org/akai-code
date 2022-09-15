c87756c9486be7d2df4d3b7de5386e60c0aa5d79
Marcin <marcinkrueger@onet.eu>
2022-09-15 21:24:12 +0200

    Add about component

--- src/components/main-page/About/About.tsx
+++ src/components/main-page/About/About.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 // import classNames from "classnames";
-// import styles from "./About.module.scss";
-import { Section, Heading, Text } from "components/ui";
+import styles from "./About.module.scss";
+import { Section, Heading, Text, Card } from "components/ui";
 
 // type Props = {};
 
@@ -9,20 +9,29 @@ export function About() {
   return (
     <Section variant="white" id="about">
       <Heading withAccent>About us</Heading>
-      <Text>
-        The first edition of the AKAI Code hackathon organized by AKAI -
-        Akademickie Koło Aplikacji Internetowych - takes place at the Poznań
-        University of Technology.
-      </Text>
-      <Text>
-        The idea of an event is simple! You code an application related to the
-        topic within 24 hours and then the jury decides which team will win.
-      </Text>
-      <Text>
-        There are three categories to compete in - Best Idea, Best Prototype and
-        Best use of technology. What&rsquo;s the topic? You&rsquo;ll find out
-        soon!
-      </Text>
+      <div className={styles.textContainer}>
+        <Text className={styles.textField}>
+          The first edition of the AKAI Code hackathon organized by AKAI -
+          Akademickie Koło Aplikacji Internetowych - takes place at the Poznań
+          University of Technology.
+        </Text>
+        <Text className={styles.textField}>
+          The idea of an event is simple! You code an application related to the
+          topic within 24 hours and then the jury decides which team will win.
+        </Text>
+        <Text className={styles.textField}>
+          There are three categories to compete in - Best Idea, Best Prototype
+          and Best use of technology. What&rsquo;s the topic? You&rsquo;ll find
+          out soon!
+        </Text>
+      </div>
+
+      <div className={styles.cardContainer}>
+        <Card name="Some name" role="Some role" />
+        <Card name="Some name" role="Some role" />
+        <Card name="Some name" role="Some role" />
+        <Card name="Some name" role="Some role" />
+      </div>
     </Section>
   );
 }
