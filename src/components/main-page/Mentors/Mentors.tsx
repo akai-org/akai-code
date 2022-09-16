ad12ed762c61a4dee08b3486235dbef51eadf741
Marcin Krueger <marcinkrueger@gmail.com>
2022-09-16 19:08:57 +0200

    Use translations inside Mentors page

--- src/components/main-page/Mentors/Mentors.tsx
+++ src/components/main-page/Mentors/Mentors.tsx
@@ -1,10 +1,13 @@
 import { Heading, Section, Card } from "components/ui";
+import useTranslation from "translations/hooks/useTranslations";
 import styles from "./Mentors.module.scss";
 
 export function Mentors() {
+  const { t } = useTranslation();
+
   return (
     <Section variant="blue" id="mentors">
-      <Heading withAccent>Mentors & Jurors</Heading>
+      <Heading withAccent>{t("mentors.title")}</Heading>
       <div className={styles.cardContainer}>
         <Card name="Some Name" company="Some Company" role="Some Role" />
         <Card name="Some Name" company="Some Company" role="Some Role" />
