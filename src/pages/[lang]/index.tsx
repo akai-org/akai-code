2e6a8cb251be108d294dc4a0fddf1ad9930830ad
roertbb <robert.banaszak1997@gmail.com>
2022-09-17 13:15:15 +0200

    extract common part of obtaining server side props

--- src/pages/[lang]/index.tsx
+++ src/pages/[lang]/index.tsx
@@ -1,6 +1,6 @@
 import { GetStaticPaths, GetStaticProps, NextPage } from "next";
-import { serverSideTranslations } from "next-i18next/serverSideTranslations";
-import { defaultLocale, locales } from "translations";
+import { locales } from "translations";
+import { getTranslationsProps } from "translations/getTranslationsProps";
 import { Index } from "../../components/main-page/Index";
 
 const IndexPage: NextPage = () => <Index />;
@@ -8,9 +8,7 @@ const IndexPage: NextPage = () => <Index />;
 export const getStaticProps: GetStaticProps = async (ctx) => {
   return {
     props: {
-      ...(await serverSideTranslations(
-        (ctx.params?.lang as string) || defaultLocale,
-      )),
+      ...(await getTranslationsProps(ctx.params?.lang as string)),
     },
   };
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,14 @@
 import React from "react";
 import { GetStaticProps, NextPage } from "next";
-import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { Index } from "components/main-page/Index";
-import { defaultLocale } from "translations";
+import { getTranslationsProps } from "translations/getTranslationsProps";
 
 const IndexPage: NextPage = () => <Index />;
 
 export const getStaticProps: GetStaticProps = async (ctx) => {
   return {
     props: {
-      ...(await serverSideTranslations(
-        (ctx.params?.lang as string) || defaultLocale,
-      )),
+      ...(await getTranslationsProps(ctx.params?.lang as string)),
     },
   };
 };
