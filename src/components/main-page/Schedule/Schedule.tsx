c98cd69223c92845640151eaeebb21a67c0bdd52
Marcin <marcinkrueger@onet.eu>
2022-10-11 00:26:16 +0200

    Create color schedule element function

--- src/components/main-page/Schedule/Schedule.tsx
+++ src/components/main-page/Schedule/Schedule.tsx
@@ -1,9 +1,11 @@
 import { useTranslation } from "next-i18next";
 import { Section, Heading, Text, ScheduleItem } from "components/ui";
 import { iconsSet } from "./../../ui/ScheduleItem/ScheduleItem";
+import { scheduleList } from "./config";
 import styles from "./Schedule.module.scss";
 
 type Agenda = {
+  id: number;
   title: string;
   icon?: string;
   start: string;
@@ -17,6 +19,29 @@ export function Schedule() {
     returnObjects: true,
   }) as { name: string; agenda: Agenda[] }[];
 
+  function createScheduleElement(item: Agenda): JSX.Element {
+    const timer =
+      scheduleList.find((el) => el.id === item.id) ?? scheduleList[0];
+    const color =
+      timer!.endTimestamp.getTime() < new Date().getTime()
+        ? "lightBlue"
+        : timer!.startTimestamp.getTime() < new Date().getTime()
+        ? "orange"
+        : "blue";
+
+    return (
+      <ScheduleItem
+        title={item.title}
+        icon={item.icon as keyof typeof iconsSet}
+        startDate={item.start}
+        endDate={item.end}
+        key={item.id}
+        theme={color}
+        className={styles.scheduleItem}
+      />
+    );
+  }
+
   return (
     <Section id="schedule">
       <Heading withAccent>{t("schedule.title")}</Heading>
@@ -25,16 +50,7 @@ export function Schedule() {
           <Text size="xl" className={styles.dayTitle}>
             {day.name}
           </Text>
-          {day.agenda.map((item) => (
-            <ScheduleItem
-              title={item.title}
-              icon={item.icon as keyof typeof iconsSet}
-              startDate={item.start}
-              endDate={item.end}
-              key={item.title}
-              className={styles.scheduleItem}
-            />
-          ))}
+          {day.agenda.map((item) => createScheduleElement(item))}
         </div>
       ))}
     </Section>
