caaea2dae1bd0cdda62286f4601e3df801cc4c56
roertbb <robert.banaszak1997@gmail.com>
2022-11-05 00:52:00 +0100

    dynamically update shchedule based on time

--- src/components/main-page/Schedule/Schedule.tsx
+++ src/components/main-page/Schedule/Schedule.tsx
@@ -1,8 +1,23 @@
 import { useTranslation } from "next-i18next";
-import { Section, Heading, Text, ScheduleItem } from "components/ui";
-import { scheduleList } from "./config";
+import { Section, Heading, Text } from "components/ui";
 import styles from "./Schedule.module.scss";
-import dayjs from "dayjs";
+import dynamic from "next/dynamic";
+import React from "react";
+
+const ScheduleElement = dynamic(
+  () =>
+    import("./ScheduleElement").then(
+      ({ ScheduleElement }) => ScheduleElement,
+    ) as Promise<
+      React.FC<{
+        elementId: string;
+        title: string;
+      }>
+    >,
+  {
+    ssr: false,
+  },
+);
 
 export function Schedule() {
   const { t } = useTranslation();
@@ -19,52 +34,18 @@ export function Schedule() {
           <Text size="xl" className={styles.dayTitle}>
             {day.name}
           </Text>
-          {Object.keys(day.eventNames).map((key) => {
-            return (
-              <ScheduleElement
-                elementId={key}
-                title={day.eventNames[key]}
-                key={key}
-              />
-            );
-          })}
+          {Object.keys(day.eventNames).map((key) => (
+            <ScheduleElement
+              elementId={key}
+              title={day.eventNames[key]}
+              key={key}
+            />
+          ))}
         </div>
       ))}
       <Text size="m" className={styles.additionalInfo}>
         {t("schedule.info")}
       </Text>
     </Section>
-  );
-}
-
-function ScheduleElement({
-  elementId,
-  title,
-}: {
-  elementId: string;
-  title: string;
-}) {
-  const config = scheduleList.find((el) => el.name === elementId);
-
-  let color: "blue" | "orange" | "lightBlue" = "blue";
-
-  if (config) {
-    const timeStart = dayjs(config.startTimestamp);
-    const timeEnd = dayjs(config.endTimestamp);
-    const now = dayjs();
-
-    if (timeEnd.isBefore(now)) color = "lightBlue";
-    else if (timeStart.isBefore(now)) color = "orange";
-  }
-
-  return (
-    <ScheduleItem
-      title={title}
-      icon={config?.icon}
-      startDate={config?.startTimeText || "XX:XX"}
-      endDate={config?.endTimeText}
-      theme={color}
-      className={styles.scheduleItem}
-    />
   );
 }
